76e5545250916fc73f7b49f83416eb89a0d5e2c8
dharmikgohil <gohildharmik2020@gmail.com>
2025-08-15 21:55:25 +0530

    feat: add ScrollToTop component to improve navigation experience

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
+import ScrollToTop from './components/ScrollToTop'
 import Home from './pages/Home'
 import About from './pages/About'
 import Services from './pages/Services'
@@ -12,6 +13,7 @@ import Contact from './pages/Contact'
 function App() {
   return (
     <div className="min-h-screen bg-white">
+      <ScrollToTop />
       <Navbar />
       <main>
         <Routes>
